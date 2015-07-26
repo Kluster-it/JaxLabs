160f4dc83950ce5999d5f697587e9d6cac6e3149
marcelolynx <marcelolynx@gmail.com>
2015-07-26 16:42:20 -0400

    first commit

---
+++ Develop/IdeaProjects/JaxLabs/src/main/java/br/com/app/repository/AnuncianteRepository.java
@@ -0,0 +1,7 @@
+package br.com.app.repository;
+
+/**
+ * Created by marcelo on 7/26/15.
+ */
+public class AnuncianteRepository {
+}
---
+++ Develop/IdeaProjects/JaxLabs/src/main/java/br/com/app/resource/AnuncianteResource.java
@@ -0,0 +1,13 @@
+package br.com.app.resource;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Created by marcelo on 7/26/15.
+ */
+public class AnuncianteResource {
+
+
+    private EntityManager em;
+}
